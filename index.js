0e237352f98e2e35ddfb24b02af887cf46416f3e
Sylvain <sylvoz75@gmail.com>
2018-12-26 22:27:48 +1100

    parsing headers

--- index.js
+++ index.js
@@ -10,11 +10,18 @@ const server = http.createServer((req, res) => {
   const path = parsedUrl.pathname;
   const trimmedPath = path.replace(/^\/+|\/+$/g, '');
 
-  // get the http method
+  // Get the query string as object (http://localhost:3000/user?name=john => query string is 'name=john'
+  const queryStringObject = parsedUrl.query;
+  // queryStringObject => { name: 'john' }
+
+  // Get the http method
   const method = req.method.toLowerCase();
 
+  // Get the header as object
+  const headers = req.headers;
+
   // Log the request path + method
-  console.log('Trimmed path + method', trimmedPath, method);
+  console.log('Trimmed path + method + queryStringObject', trimmedPath, method, queryStringObject, 'headers:', headers);
 
   // Send the response
   res.end('hello\n')
